b8ed10c6cbface795491fcc83ee67d3ab918318c
reyoung <reyoung@126.com>
2011-06-04 21:22:21 +0800

    Rat basically complete. start Scenario.java

--- Solapop/src/tjuhot/solapop/GameMain.java
+++ Solapop/src/tjuhot/solapop/GameMain.java
@@ -1,21 +1,57 @@
 package tjuhot.solapop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.anddev.andengine.engine.Engine;
 import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.handler.timer.ITimerCallback;
+import org.anddev.andengine.engine.handler.timer.TimerHandler;
 import org.anddev.andengine.engine.options.EngineOptions;
 import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureFactory;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
 import org.anddev.andengine.ui.activity.BaseGameActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class GameMain extends BaseGameActivity {
 
 	private static final int CAMERA_WIDTH = 720;
 	private static final int CAMERA_HEIGHT = 480;
+	private static final String DB_FILTER = "GameMain";
+	
+	private static final String STR_RAT_PATH[] = {
+		"keng",
+		"rat1",
+		"rat2",
+		"rat3",
+		"rat4",
+		"rat5",
+		"rat6"
+	};
+	public static final String STR_HITRAT = "hitrat";
+	
+	List<TextureRegion> mRatTextureRegions;
+	List<Texture>       mRatTextures;
+	
+	List<TextureRegion> mHitRatTextureRegions;
+	List<Texture>       mHitRatTextures;
 	Camera mCamera;
+
+	void debug(String msg) {
+		Log.d(DB_FILTER, msg);
+	}
 
 	public void onLoadComplete() {
 
@@ -23,19 +59,59 @@ public class GameMain extends BaseGameActivity {
 
 	public Engine onLoadEngine() {
 		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-		return new Engine(new EngineOptions(true,ScreenOrientation.LANDSCAPE
-				,new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera)
-				.setNeedsSound(true).setNeedsMusic(true));
+		return new Engine(
+				new EngineOptions(true, ScreenOrientation.LANDSCAPE,
+						new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT),
+						mCamera).setNeedsSound(true).setNeedsMusic(true));
 	}
 
 	public void onLoadResources() {
-		// TODO Auto-generated method stub
-
+		loadRatTexture();
+		loadHitRatTexture();
 	}
-
+	void loadHitRatTexture(){
+		mHitRatTextureRegions = new ArrayList<TextureRegion>();
+		mHitRatTextures = new ArrayList<Texture>();
+		for(int i=1;i<STR_RAT_PATH.length;++i){
+			Texture t = new Texture(128, 128);
+			TextureRegion temp = TextureRegionFactory.createFromAsset(t
+					, getApplicationContext(), String.format("gfx/%s%d.png", STR_HITRAT,i),0,0);
+			mHitRatTextureRegions.add(temp);
+			mHitRatTextures.add(t);
+			this.mEngine.getTextureManager().loadTexture(t);
+		}
+	}
+	void loadRatTexture(){
+		mRatTextureRegions = new ArrayList<TextureRegion>();
+		mRatTextures = new ArrayList<Texture>();
+		for(int i=0;i<STR_RAT_PATH.length;++i){
+			Texture tempt = new Texture(128, 128);
+			TextureRegion temp = TextureRegionFactory.createFromAsset(tempt
+					, getApplicationContext(), String.format("gfx/%s.png", STR_RAT_PATH[i]),0,0);
+			mRatTextureRegions.add(temp);
+			mRatTextures.add(tempt);
+			this.mEngine.getTextureManager().loadTexture(tempt);
+			debug(String.format("gfx/%s.png", STR_RAT_PATH[i]));
+		}
+		debug(String.format("RatTexture size = %d", mRatTextureRegions.size()));
+	}
+	
+	
 	public Scene onLoadScene() {
-		// TODO Auto-generated method stub
-		return null;
+		debug("OnLoadScense");
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+		final Scene scene = new Scene(1);
+		final Scenario sc = new Scenario(mRatTextureRegions, mHitRatTextureRegions);
+		scene.getLastChild().attachChild(sc);
+		sc.addRat(100, 100);
+		sc.addRat(150, 150);
+		sc.addRat(200, 200);
+		scene.registerUpdateHandler(new TimerHandler( 0.5f , true,new ITimerCallback() {
+			public void onTimePassed(TimerHandler pTimerHandler) {
+				sc.step();
+			}
+		}));
+		return scene;    
 	}
 
 }
---
+++ Solapop/src/tjuhot/solapop/Rat.java
@@ -0,0 +1,66 @@
+package tjuhot.solapop;
+
+import java.util.List;
+
+import org.anddev.andengine.entity.Entity;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+
+import android.util.Log;
+
+public class Rat extends Entity {
+	List<TextureRegion> mTextures;
+	List<TextureRegion> mHitTextures;
+	Sprite mSprite;
+	int mPos;
+	int mIncrese;
+	private static final int INCRESE = 0;
+	private static final int DECRESE = 1;
+	private static final int HITTED = 2;
+
+	private static final String DB_FILTER = "Solapop.Rat";
+
+	void debug(String msg) {
+		Log.d(DB_FILTER, msg);
+	}
+
+	public Rat(List<TextureRegion> aTextures, List<TextureRegion> aHitTextures) {
+		super(0, 0);
+		mTextures = aTextures;
+		mHitTextures = aHitTextures;
+		mPos = 0;
+		mIncrese = INCRESE;
+		mSprite = new Sprite(0, 0, mTextures.get(0));
+		this.attachChild(mSprite);
+	}
+
+	public void showNext() {
+		debug("Show Next");
+		if (mIncrese == INCRESE) {
+			++mPos;
+		} else if (mIncrese == DECRESE) {
+			--mPos;
+		}
+		if (mPos >= mTextures.size() - 1) {
+			mPos = mTextures.size() - 1;
+			mIncrese = DECRESE;
+		}
+		if (mPos <= 0) {
+			mPos = 0;
+			mIncrese = INCRESE;
+		}
+		this.detachChild(mSprite);
+		if (HITTED == mIncrese) {
+			mIncrese = DECRESE;
+			if(mPos ==0)
+				return;
+			mSprite = new Sprite(0, 0, mHitTextures.get(mPos-1));
+		} else
+			mSprite = new Sprite(0, 0, mTextures.get(mPos));
+		this.attachChild(mSprite);
+	}
+
+	public void hit() {
+		mIncrese = HITTED;
+	}
+}
