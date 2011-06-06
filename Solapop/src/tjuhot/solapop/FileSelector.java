fd57d591567184170dd1a9b58eb3cc88fc5f76d0
XuDaWei <lordvv@xnmsn.cn>
2011-06-06 13:49:58 +0800

    select

---
+++ Solapop/src/tjuhot/solapop/FileSelector.java
@@ -0,0 +1,119 @@
+package tjuhot.solapop;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class FileSelector extends ListActivity {
+
+	private List<String> items = null;
+	private List<String> itemsMask = null;
+	private String filePath = null;
+	private String homePath = null;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		
+		String status = Environment.getExternalStorageState();
+		  if (status.equals(Environment.MEDIA_MOUNTED)) {
+			  homePath="mnt/sdcard/"; 
+		        File file=new File(homePath+"SolaPop"); 
+		        if(!file.exists()) 
+		         file.mkdir();
+		  } else {
+			  Toast.makeText(this, "sdcard not exist,choose slp file yourslef!", Toast.LENGTH_LONG).show();
+			  homePath="/";
+		  }
+		
+		  filePath = homePath;
+		  fillList(new File(filePath).listFiles());
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		filePath = items.get((int) id);
+		File file = new File(filePath);
+		if (file.isDirectory()) {
+			fillList(file.listFiles());
+		} else{
+			Toast.makeText(FileSelector.this, ""+filePath, Toast.LENGTH_SHORT)
+					.show();
+			Intent intent= new Intent(FileSelector.this,GameMain.class);
+			startActivity(intent);
+		}
+	}
+
+	private void fillList(File[] files) {
+		items = new ArrayList<String>();
+		itemsMask = new ArrayList<String>();
+		// 遍例整个files目录下的所有文件及文件夹
+		Log.e("TAG", "rec file: "+filePath);
+		Log.e("TAG", "rec home: "+homePath);
+		if(filePath!=homePath&&filePath.length()>homePath.length()){
+			String parentStr=new File(filePath).getParent();
+			items.add(parentStr);
+			itemsMask.add("..");
+			
+		}
+		boolean existslp=false;
+		for (File file : files){
+			if(file.isDirectory()||file.getName().toLowerCase().endsWith(".slp")){
+				existslp=true;
+			}
+		}
+		if(existslp==false)
+		{
+			Toast.makeText(FileSelector.this, "NO SLP FILE！", Toast.LENGTH_SHORT)
+			.show();
+		}
+		
+		for (File file : files) {
+			if (file.isDirectory()
+					|| file.getName().toLowerCase().endsWith(".slp")){
+				items.add(file.getPath().toString());
+				String p=file.getPath().toString();
+				for(int i=p.length()-1;i>0;i--){
+					if(p.charAt(i)=='/')
+					{
+						p=p.substring(i+1,p.length());
+						break;
+					}
+				}
+				itemsMask.add(p);
+			}
+		}
+		ArrayAdapter<String> fileList = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, itemsMask);
+		setListAdapter(fileList);
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		switch (keyCode) {
+		case KeyEvent.KEYCODE_BACK:
+			if ((filePath).equals(homePath)) {
+				this.finish();
+				break;
+			} else {
+				filePath = new File(filePath).getParent();
+				fillList(new File(filePath).listFiles());
+				
+				return true;
+			}
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+}
--- Solapop/src/tjuhot/solapop/GameMenu.java
+++ Solapop/src/tjuhot/solapop/GameMenu.java
@@ -9,6 +9,7 @@ import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.anddev.andengine.entity.modifier.MoveModifier;
 import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.Scene.IOnSceneTouchListener;
 import org.anddev.andengine.entity.scene.background.ColorBackground;
 import org.anddev.andengine.entity.scene.menu.MenuScene;
 import org.anddev.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
@@ -16,6 +17,7 @@ import org.anddev.andengine.entity.scene.menu.item.IMenuItem;
 import org.anddev.andengine.entity.scene.menu.item.SpriteMenuItem;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.input.touch.TouchEvent;
 import org.anddev.andengine.opengl.texture.Texture;
 import org.anddev.andengine.opengl.texture.TextureOptions;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
@@ -25,31 +27,28 @@ import org.anddev.andengine.ui.activity.BaseGameActivity;
 import tjuhot.solapop.GameMain;
 
 import android.content.Intent;
+import android.util.Log;
 import android.view.KeyEvent;
 
 public class GameMenu extends BaseGameActivity  implements IOnMenuItemClickListener {
-	// ===========================================================
-	// Constants
-	// ===========================================================
 
-	private static final int CAMERA_WIDTH = 1024;
-	private static final int CAMERA_HEIGHT = 600;
-
+	private static final int CAMERA_WIDTH = 512;
+	private static final int CAMERA_HEIGHT = 300;
+	private static final int BUTTON_WIDTH = 150;
+	private static final int BUTTON_HEIGHT = 75;
 	protected static final int MENU_OK = 0;
 	protected static final int MENU_RESET = MENU_OK + 1;
 	protected static final int MENU_QUIT = MENU_RESET + 1;
 	
 
-	// ===========================================================
-	// Fields
-	// ===========================================================
+
 
 	protected Camera mCamera;
 
 	protected Scene mMainScene;
 
 	private Texture mTexture;
-	private Engine mEngine;
+
 	private TextureRegion mFaceTextureRegion;
 
 	protected MenuScene mMenuScene;
@@ -58,18 +57,6 @@ public class GameMenu extends BaseGameActivity  implements IOnMenuItemClickListe
 	protected TextureRegion mMenuResetTextureRegion;
 	protected TextureRegion mMenuQuitTextureRegion;
 	protected TextureRegion mMenuOKTextureRegion;
-
-	// ===========================================================
-	// Constructors
-	// ===========================================================
-
-	// ===========================================================
-	// Getter & Setter
-	// ===========================================================
-
-	// ===========================================================
-	// Methods for/from SuperClass/Interfaces
-	// ===========================================================
 
 
 	public Engine onLoadEngine() {
@@ -83,19 +70,25 @@ public class GameMenu extends BaseGameActivity  implements IOnMenuItemClickListe
 	public void onLoadResources() {
 		this.mTexture = new Texture(64, 64,
 				TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+
 		this.mFaceTextureRegion = TextureRegionFactory.createFromAsset(
 				this.mTexture, this, "gfx/face_box_menu.png", 0, 0);
-		this.mEngine.getTextureManager().loadTexture(this.mTexture);
 
+		this.mEngine.getTextureManager().loadTexture(this.mTexture);
+		
 		this.mMenuTexture = new Texture(256, 128,
 				TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+
 		this.mMenuResetTextureRegion = TextureRegionFactory.createFromAsset(
 				this.mMenuTexture, this, "gfx/menu_reset.png", 0, 0);
+
 		this.mMenuQuitTextureRegion = TextureRegionFactory.createFromAsset(
-				this.mMenuTexture, this, "gfx/menu_quit.png", 0, 50);
+				this.mMenuTexture, this, "gfx/menu_quit.png", 0, 0);
+
 		this.mMenuOKTextureRegion = TextureRegionFactory.createFromAsset(
-				this.mMenuTexture, this, "gfx/menu_ok.png", 0, 100);
-		this.mEngine.getTextureManager().loadTexture(this.mMenuTexture);
+				this.mMenuTexture, this, "gfx/menu_ok.png", 0, 50);
+
+		this.mEngine.getTextureManager().loadTexture(this.mMenuTexture);		
 	}
 
 
@@ -103,16 +96,46 @@ public class GameMenu extends BaseGameActivity  implements IOnMenuItemClickListe
 		this.mEngine.registerUpdateHandler(new FPSLogger());
 
 		this.createMenuScene();
-
+		Log.e("TAG", "Step1");
 		/* Just a simple scene with an animated face flying around. */
 		this.mMainScene = new Scene(1);
 		this.mMainScene.setBackground(new ColorBackground(0.09804f, 0.6274f,
 				0.8784f));
 
-		final Sprite face = new Sprite(0, 0, this.mFaceTextureRegion);
-		face.registerEntityModifier(new MoveModifier(30, 0, CAMERA_WIDTH
-				- face.getWidth(), 0, CAMERA_HEIGHT - face.getHeight()));
-		this.mMainScene.getLastChild().attachChild(face);
+		
+//		final Sprite face = new Sprite(0,50, this.mFaceTextureRegion);
+//		face.registerEntityModifier(new MoveModifier(30, 0, CAMERA_WIDTH
+//				- face.getWidth(), 0, CAMERA_HEIGHT - face.getHeight()));
+//		this.mMainScene.getLastChild().attachChild(face);
+		
+		final Sprite button1 = new Sprite(BUTTON_WIDTH,BUTTON_HEIGHT, this.mMenuOKTextureRegion);
+		this.mMainScene.getLastChild().attachChild(button1);
+		mMainScene.setOnSceneTouchListener(new IOnSceneTouchListener() {
+			public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+				float x = pSceneTouchEvent.getX();
+				float y = pSceneTouchEvent.getY();
+				if(x>BUTTON_WIDTH&&x<(BUTTON_WIDTH+256)&&y>BUTTON_HEIGHT&&y<(BUTTON_HEIGHT+128)){
+					Intent intent= new Intent(GameMenu.this,FileSelector.class);
+					startActivity(intent);
+				}
+				return false;
+			}
+		});
+		
+		
+//		final Sprite button2 = new Sprite(BUTTON_WIDTH,BUTTON_HEIGHT+100, this.mMenuQuitTextureRegion);
+//		this.mMainScene.getLastChild().attachChild(button2);
+//		mMainScene.setOnSceneTouchListener(new IOnSceneTouchListener() {
+//			public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+//				float x = pSceneTouchEvent.getX();
+//				float y = pSceneTouchEvent.getY();
+//				if(x>BUTTON_WIDTH&&x<(BUTTON_WIDTH+256)&&y>(BUTTON_HEIGHT+100)&&y<(BUTTON_HEIGHT+128+100)){
+//					System.exit(0);
+//				}
+//				return false;
+//			}
+//		});
+		
 
 		return this.mMainScene;
 	}
@@ -144,6 +167,13 @@ public class GameMenu extends BaseGameActivity  implements IOnMenuItemClickListe
 	public boolean onMenuItemClicked(final MenuScene pMenuScene,
 			final IMenuItem pMenuItem, final float pMenuItemLocalX,
 			final float pMenuItemLocalY) {
+		
+		Log.e("TAG","press");
+		if(pMenuItemLocalX>50&&pMenuItemLocalY>50&&pMenuItemLocalX<100&&pMenuItemLocalY<100){
+			Intent intent= new Intent(GameMenu.this,GameMain.class);
+			startActivity(intent);
+		}
+		
 		switch (pMenuItem.getID()) {
 		case MENU_OK:
 			Intent intent= new Intent(GameMenu.this,GameMain.class);
@@ -173,7 +203,7 @@ public class GameMenu extends BaseGameActivity  implements IOnMenuItemClickListe
 		this.mMenuScene = new MenuScene(this.mCamera);
 
 		final SpriteMenuItem okMenuItem = new SpriteMenuItem(MENU_OK,
-				this.mMenuResetTextureRegion);
+				this.mMenuOKTextureRegion);
 		okMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA,
 				GL10.GL_ONE_MINUS_SRC_ALPHA);
 		this.mMenuScene.addMenuItem(okMenuItem);
@@ -183,13 +213,13 @@ public class GameMenu extends BaseGameActivity  implements IOnMenuItemClickListe
 		resetMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA,
 				GL10.GL_ONE_MINUS_SRC_ALPHA);
 		this.mMenuScene.addMenuItem(resetMenuItem);
-
+		
 		final SpriteMenuItem quitMenuItem = new SpriteMenuItem(MENU_QUIT,
 				this.mMenuQuitTextureRegion);
 		quitMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA,
 				GL10.GL_ONE_MINUS_SRC_ALPHA);
 		this.mMenuScene.addMenuItem(quitMenuItem);
-
+		
 		this.mMenuScene.buildAnimations();
 
 		this.mMenuScene.setBackgroundEnabled(false);
